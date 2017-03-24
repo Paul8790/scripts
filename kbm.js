f1edf8caceb4a17ac53138a42f9f8b3fcabf6524
Paul8790 <pcmore7@gmail.com>
2017-03-24 17:19:53 -0300

    Create kbm.js

---
+++ kbm.js
@@ -0,0 +1,41 @@
+<script language='JavaScript'>
+imgr = new Array();
+
+imgr[0] = 'URL DA 1ª IMAGEM';
+imgr[1] = 'URL DA 1ª IMAGEM';
+imgr[2] = 'URL DA 1ª IMAGEM';
+imgr[3] = 'URL DA 1ª IMAGEM';
+imgr[4] = 'URL DA 1ª IMAGEM';
+
+showRandomImg = true;
+
+tablewidth = 220;
+cellspacing = 2;
+borderColor = '#ffffff';
+bgTD = ''
+
+imgwidth = 250;
+imgheight = 250;
+
+fntsize = 18;
+acolor = '#2c343b';
+aBold = true;
+icon = '';
+
+text = '';
+
+showcmtext = false;
+showPostDate = false;
+
+summaryPost = 120;
+summaryFontsize = 16;
+summaryColor = '#001f44';
+icon2 = '';
+
+numposts = 4;
+
+label = 'Atuação';
+
+home_page = 'http://www.kbm.adv.br';
+
+</script>
