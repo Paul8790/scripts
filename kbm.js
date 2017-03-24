fdea3bbb11bb5a75177870f40329741d8cf18eac
Paul8790 <pcmore7@gmail.com>
2017-03-24 17:29:26 -0300

    Update kbm.js

--- kbm.js
+++ kbm.js
@@ -1,41 +1,38 @@
-<script language='JavaScript'>
 imgr = new Array();
 
-imgr[0] = 'URL DA 1ª IMAGEM';
-imgr[1] = 'URL DA 1ª IMAGEM';
-imgr[2] = 'URL DA 1ª IMAGEM';
-imgr[3] = 'URL DA 1ª IMAGEM';
-imgr[4] = 'URL DA 1ª IMAGEM';
+imgr[0] = "URL DA 1ª IMAGEM";
+imgr[1] = "URL DA 1ª IMAGEM";
+imgr[2] = "URL DA 1ª IMAGEM";
+imgr[3] = "URL DA 1ª IMAGEM";
+imgr[4] = "URL DA 1ª IMAGEM";
 
 showRandomImg = true;
 
 tablewidth = 220;
 cellspacing = 2;
-borderColor = '#ffffff';
-bgTD = ''
+borderColor = "#ffffff";
+bgTD = ""
 
 imgwidth = 250;
 imgheight = 250;
 
 fntsize = 18;
-acolor = '#2c343b';
+acolor = "#2c343b";
 aBold = true;
-icon = '';
+icon = "";
 
-text = '';
+text = "";
 
 showcmtext = false;
 showPostDate = false;
 
 summaryPost = 120;
 summaryFontsize = 16;
-summaryColor = '#001f44';
-icon2 = '';
+summaryColor = "#001f44";
+icon2 = "";
 
 numposts = 4;
 
-label = 'Atuação';
+label = "Atuação";
 
 home_page = 'http://www.kbm.adv.br';
-
-</script>
